d84fa3ecbc1db3d5ab292eec9de6ea65a81a100f
Biju Kunjummen <biju.kunjummen@gmail.com>
2017-07-18 21:21:23 -0700

    Sample with kotlintest and scalacheck

---
+++ src/test/kotlin/sample/SamplePropTest.kt
@@ -0,0 +1,26 @@
+package sample
+
+import io.kotlintest.properties.forAll
+import io.kotlintest.specs.StringSpec
+
+class StringSpecs: StringSpec() {
+    init {
+        "startsWith" {
+            forAll { a: String, b: String ->
+                (a + b).startsWith(a)
+            }
+        }
+        
+        "concatenate" { 
+            forAll { a: String, b: String ->
+                (a + b).length > a.length && (a + b).length > b.length
+            }
+        }
+        
+        "substring" {
+            forAll { a: String, b: String, c: String -> 
+                (a + b + c).substring(a.length, a.length + b.length) == b
+            }
+        }
+    }
+}
